a612f25c21314e8e1f48f744b76fe81f0fb626ae
rubio <rubio@192.168.236.1>
2013-10-16 23:26:01 +0200

    venga

---
+++ nuevoprogramacion/src/nuevoprogramacion/Ejercicios1.java
@@ -0,0 +1,23 @@
+package nuevoprogramacion;
+
+
+
+	import java.util.Scanner;
+
+	public class Ejercicios1 {
+
+		@SuppressWarnings("unused")
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner teclado=new Scanner(System.in);
+			String  nombre;
+			
+			System.out.println("introduce no nombre: ");
+			nombre=teclado.nextLine();
+			System.out.println("bienvenido!!");
+		}
+
+
+	}
+
+
